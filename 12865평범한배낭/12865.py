2e458fe88f5218be6894ad00cb34f4de3b02c70a
junholee <juno7803@naver.com>
2021-01-29 23:14:28 +0900

    feat: 12865 평범한 배낭 코드 구현

---
+++ 12865평범한배낭/12865.py
@@ -0,0 +1,17 @@
+from sys import stdin
+input = stdin.readline
+
+n, k = map(int, input().split())
+item = [[0, 0]]
+for i in range(1, n+1):
+    item.append(list(map(int, input().split())))
+dp = [[0]*(k+1) for _ in range(n+1)]  # (n+1) x (k+1) matrix
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        if j >= item[i][0]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-item[i][0]] + item[i][1])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[n][k])
---
+++ 2110공유기설치/2110.py
@@ -0,0 +1,7 @@
+from sys import stdin
+input = stdin.readline
+
+n, m = map(int, input().split())
+arr = []
+for i in range(n):
+    arr.append(int(input()))
