abc682c96df3f3ab5b3e07dc5be6a31ad07abbfb
junholee <juno7803@naver.com>
2021-03-12 17:40:32 +0900

    feat: 2606 바이러스 코드 구현

--- GRAPH/2606바이러스/2606.py
+++ GRAPH/2606바이러스/2606.py
@@ -1,31 +1,29 @@
 from sys import stdin
 input = stdin.readline
 
+
+def dfs(v):
+    visited[v] = 1
+    for i in adj[v]:
+        if not visited[i]:
+            dfs(i)
+
+
 n = int(input())
 m = int(input())
-visited = [False] * n  # 방문 기록을 위한 list
-adj = [[] for _ in range(n+1)]
-cnt = 0
+visited = [0 for _ in range(n+1)]  # 방문 기록을 위한 list
+adj = [[] for _ in range(n+1)]  # zero padding
 
-
-def dfs(graph, v, visited):
-    # 현재 노드를 방문 처리
-    visited[v] = True
-    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
-
-
-for _ in range(m):
+for _ in range(m):  # 인접 리스트 할당
     x, y = map(int, input().split())
     adj[x].append(y)
     adj[y].append(x)
 
-dfs(adj, 1, visited)  # 호출
 
-for i in range(n):
-    if visited[i] == True:
+dfs(1)  # 호출
+cnt = 0
+for i in visited:
+    if i == 1:
         cnt += 1
 
-print(cnt - 1)  # 1 자기자신 제외
+print(cnt-1)  # 시작점인 1 제외
