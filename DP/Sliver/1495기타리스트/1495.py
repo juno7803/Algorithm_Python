22d0b25e150a50d2fa0e7042b6d980ea7df54df0
junholee <juno7803@naver.com>
2021-03-12 15:43:38 +0900

    feat: 11048 이동하기 구현, 설명 추가

--- DP/Sliver/1495기타리스트/1495.py
+++ DP/Sliver/1495기타리스트/1495.py
@@ -8,11 +8,11 @@ visited[0][s] = 1  # 초기값 설정
 
 for i in range(1, n+1):
     for j in range(m+1):
-        if visited[i-1][j] == 0:
+        if visited[i-1][j] == 0:  # 0일 경우 pass
             continue
-        if j + arr[i-1] <= m:
+        if j + arr[i-1] <= m:  # 배열안의 볼륨 값을 더했을 때 m 이하이면 연주 가능
             visited[i][j+arr[i-1]] = 1
-        if j - arr[i-1] >= 0:
+        if j - arr[i-1] >= 0:  # 배열안의 볼륨 값을 뺐을 때 0 이상이면 연주 가능
             visited[i][j-arr[i-1]] = 1
 
 ans = -1  # 연주할 수 없는 경우 : "-1"
